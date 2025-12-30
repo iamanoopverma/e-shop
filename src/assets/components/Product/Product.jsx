bdc19f297c172d8a0c7fb345be37f3fb4c6e2e4b
Anoop Verma <iamanoopverma@gmail.com>
2025-12-31 00:42:54 +0530

    Version 1.1

---
+++ src/assets/components/Product/Product.jsx
@@ -0,0 +1,13 @@
+import 'Product.css';
+
+function Product() {
+    return (
+        <>
+            <div className="product-card">
+                <img src="" alt="" />
+                <div className="title"></div>
+                <div className="price"></div>           
+            </div>
+        </>
+    );
+}
