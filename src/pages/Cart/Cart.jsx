dc003fecad1aaae70aa84fee3d66523902bc4b94
Anoop Verma <iamanoopverma@gmail.com>
2026-01-16 04:52:42 +0530

    fix: return cart total correctly and hide total for empty cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -2,16 +2,16 @@ import { useCart } from '../../context/CartContext'
 
 function Cart() {
   const { cart, addToCart, decreaseProductQty } = useCart();
-  function total() { 
-    cart.reduce((acc, item) => (acc + (item.price * item.qty)), 0) 
+  function total() {
+    return cart.reduce((acc, item) => (acc + (item.price * item.qty)), 0)
   }
 
   return (
     <>
       <h2>Cart</h2>
 
-      {cart.length === 0 ? <p>Cart is Empty.</p> :
-        cart.map(item => (
+      {cart.length > 0 ?
+        <div>{cart.map(item => (
           <div key={item.id} className='cart-item'>
             <img src={item.image} alt={item.title} width="120" />
             <p>{item.title}</p>
@@ -23,8 +23,14 @@ function Cart() {
             </div>
           </div>
         ))}
+        <h2>Total:{total()} </h2>
+        </div>
+        :
+        <div>
+          <p>Cart is Empty.</p>
+        </div>
+      }
 
-      <h2>Total:{total()} </h2>
     </>
   );
 }
