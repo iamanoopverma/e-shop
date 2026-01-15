bf050f368ab774f2c80feb4b6e04bfb78b440cab
Anoop Verma <iamanoopverma@gmail.com>
2026-01-16 04:13:47 +0530

    feat: add plus icon on homepage products for add to cart

--- src/assets/components/Product/Product.jsx
+++ src/assets/components/Product/Product.jsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom'
+import {useCart} from '../../../context/CartContext'
 import './Product.css';
 
 function Product({ item }) {
+   const {cart, addToCart} = useCart();
+
     return (
         <>
             <div className="product-card">
+                <button onClick={() => addToCart(item.id, item)}>+</button>
                 <Link to={`/product/${item.id}`}>
                     <img src={item.image} alt={item.title} width="100" />
                 </Link>
